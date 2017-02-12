b7b3e4a7ad98830f290fa0c486fc9af29b9064ff
linbaoming <baoming_lin@163.com>
2017-02-12 12:42:31 +0800

    复用类

---
+++ src/main/java/com/christer/thinkingInJava/reusing/BlankFinal.java
@@ -0,0 +1,30 @@
+package com.christer.thinkingInJava.reusing;//: reusing/BlankFinal.java
+// "Blank" final fields.
+
+class Poppet {
+  private int i;
+  Poppet(int ii) { i = ii; }
+}
+
+/**
+ * 编译器会确保在final使用前必须进行初始化
+ * 所以正确的用法，必须在域的定义处或类的构造器中对final域进行初始化
+ */
+public class BlankFinal {
+  private final int i = 0; // Initialized final
+  private final int j; // Blank final
+  private final Poppet p; // Blank final reference
+  // Blank finals MUST be initialized in the constructor:
+  public BlankFinal() {
+    j = 1; // Initialize blank final
+    p = new Poppet(1); // Initialize blank final reference
+  }
+  public BlankFinal(int x) {
+    j = x; // Initialize blank final
+    p = new Poppet(x); // Initialize blank final reference
+  }
+  public static void main(String[] args) {
+    new BlankFinal();
+    new BlankFinal(47);
+  }
+} ///:~

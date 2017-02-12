1dc93fe7c044f041ca3602b27c8f7132e5faeb81
linbaoming <baoming_lin@163.com>
2017-02-13 00:14:00 +0800

    内部类

---
+++ src/main/java/com/christer/thinkingInJava/innerclasses/Contents.java
@@ -0,0 +1,6 @@
+package com.christer.thinkingInJava.innerclasses;
+
+//: innerclasses/Contents.java
+public interface Contents {
+  int value();
+} ///:~
---
+++ src/main/java/com/christer/thinkingInJava/innerclasses/Destination.java
@@ -0,0 +1,6 @@
+package com.christer.thinkingInJava.innerclasses;
+
+//: innerclasses/Destination.java
+public interface Destination {
+  String readLabel();
+} ///:~

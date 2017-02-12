c8b325a7158b9ec68f5a8295af85931ed40e2226
linbaoming <baoming_lin@163.com>
2017-02-12 16:20:09 +0800

    接口

---
+++ src/main/java/com/christer/thinkingInJava/interfaces/interfaceprocessor/Processor.java
@@ -0,0 +1,7 @@
+//: interfaces/interfaceprocessor/Processor.java
+package com.christer.thinkingInJava.interfaces.interfaceprocessor;
+
+public interface Processor {
+  String name();
+  Object process(Object input);
+} ///:~

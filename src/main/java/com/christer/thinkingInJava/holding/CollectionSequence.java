78654dbd7b8f1667c79def13ce5427bfe5d42f2b
linbaoming <baoming_lin@163.com>
2017-02-15 00:47:05 +0800

    持有对象

---
+++ src/main/java/com/christer/thinkingInJava/holding/CollectionSequence.java
@@ -0,0 +1,34 @@
+package com.christer.thinkingInJava.holding;//: holding/CollectionSequence.java
+import com.christer.thinkingInJava.typeinfo.pets.*;
+import java.util.*;
+
+/**
+ * 实现AbstractCollection就必须实现iterator()
+ * 如果继承了AbstractCollection类就不能继承其它类
+ * 所以如果已继承了其它类就只能通过生成Iterator来实现了
+ */
+public class CollectionSequence
+extends AbstractCollection<Pet> {
+  private Pet[] pets = Pets.createArray(8);
+  public int size() { return pets.length; }
+  public Iterator<Pet> iterator() {
+    return new Iterator<Pet>() {
+      private int index = 0;
+      public boolean hasNext() {
+        return index < pets.length;
+      }
+      public Pet next() { return pets[index++]; }
+      public void remove() { // Not implemented
+        throw new UnsupportedOperationException();
+      }
+    };
+  }	
+  public static void main(String[] args) {
+    CollectionSequence c = new CollectionSequence();
+    InterfaceVsIterator.display(c);
+    InterfaceVsIterator.display(c.iterator());
+  }
+} /* Output:
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+*///:~

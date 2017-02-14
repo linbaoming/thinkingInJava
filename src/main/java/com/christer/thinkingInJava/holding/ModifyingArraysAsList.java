7dbf885b18267cbf499782349aaf6cf5a785a6bd
linbaoming <baoming_lin@163.com>
2017-02-15 01:13:01 +0800

    持有对象

---
+++ src/main/java/com/christer/thinkingInJava/holding/ModifyingArraysAsList.java
@@ -0,0 +1,31 @@
+package com.christer.thinkingInJava.holding;//: holding/ModifyingArraysAsList.java
+import java.util.*;
+
+/**
+ * 1跟2 的不同之处在于1生成的List复制传递给ArrayList使用，而2生成的List直接赋值给List
+ */
+public class ModifyingArraysAsList {
+  public static void main(String[] args) {
+    Random rand = new Random(47);
+    Integer[] ia = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    List<Integer> list1 =
+      new ArrayList<Integer>(Arrays.asList(ia)); // 1
+    System.out.println("Before shuffling: " + list1);
+    Collections.shuffle(list1, rand);
+    System.out.println("After shuffling: " + list1);
+    System.out.println("array: " + Arrays.toString(ia));
+
+    List<Integer> list2 = Arrays.asList(ia); // 2
+    System.out.println("Before shuffling: " + list2);
+    Collections.shuffle(list2, rand);
+    System.out.println("After shuffling: " + list2);
+    System.out.println("array: " + Arrays.toString(ia));
+  }
+} /* Output:
+Before shuffling: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+After shuffling: [4, 6, 3, 1, 8, 7, 2, 5, 10, 9]
+array: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+Before shuffling: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+After shuffling: [9, 1, 6, 3, 7, 2, 5, 10, 4, 8]
+array: [9, 1, 6, 3, 7, 2, 5, 10, 4, 8]
+*///:~

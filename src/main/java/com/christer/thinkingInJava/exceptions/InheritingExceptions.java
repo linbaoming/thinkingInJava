db4b1b9fb437cc96c8fb17c62bc5f7a932708554
linbaoming <baoming_lin@163.com>
2017-02-20 22:41:42 +0800

    通过异常处理错误

---
+++ src/main/java/com/christer/thinkingInJava/exceptions/InheritingExceptions.java
@@ -0,0 +1,25 @@
+package com.christer.thinkingInJava.exceptions;//: exceptions/InheritingExceptions.java
+// Creating your own exceptions.
+
+/**
+ * 自定义异常
+ */
+class SimpleException extends Exception {}
+
+public class InheritingExceptions {
+  public void f() throws SimpleException {
+    System.out.println("Throw SimpleException from f()");
+    throw new SimpleException();
+  }
+  public static void main(String[] args) {
+    InheritingExceptions sed = new InheritingExceptions();
+    try {
+      sed.f();
+    } catch(SimpleException e) {
+      System.out.println("Caught it!");
+    }
+  }
+} /* Output:
+Throw SimpleException from f()
+Caught it!
+*///:~

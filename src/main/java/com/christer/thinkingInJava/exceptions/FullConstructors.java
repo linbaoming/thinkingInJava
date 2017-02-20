284ed94e39cd5ca87bad992fbade43633981be2a
linbaoming <baoming_lin@163.com>
2017-02-20 22:54:17 +0800

    通过异常处理错误

---
+++ src/main/java/com/christer/thinkingInJava/exceptions/FullConstructors.java
@@ -0,0 +1,41 @@
+package com.christer.thinkingInJava.exceptions;//: exceptions/FullConstructors.java
+
+/**
+ * 自定义异常，带有参构造函数
+ */
+class MyException extends Exception {
+  public MyException() {}
+  public MyException(String msg) { super(msg); }
+}
+
+public class FullConstructors {
+  public static void f() throws MyException {
+    System.out.println("Throwing MyException from f()");
+    throw new MyException();
+  }
+  public static void g() throws MyException {
+    System.out.println("Throwing MyException from g()");
+    throw new MyException("Originated in g()");
+  }
+  public static void main(String[] args) {
+    try {
+      f();
+    } catch(MyException e) {
+      e.printStackTrace(System.out);// 向System.out输出从方法调用处直到异常抛出处
+    }
+    try {
+      g();
+    } catch(MyException e) {
+      e.printStackTrace(System.out);
+    }
+  }
+} /* Output:
+Throwing MyException from f()
+MyException
+        at FullConstructors.f(FullConstructors.java:11)
+        at FullConstructors.main(FullConstructors.java:19)
+Throwing MyException from g()
+MyException: Originated in g()
+        at FullConstructors.g(FullConstructors.java:15)
+        at FullConstructors.main(FullConstructors.java:24)
+*///:~

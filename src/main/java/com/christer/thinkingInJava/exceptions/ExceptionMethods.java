f8d5f01e8c6f1d948b6e2031353aaa76e448bce0
linbaoming <baoming_lin@163.com>
2017-02-20 23:36:09 +0800

    通过异常处理错误

---
+++ src/main/java/com/christer/thinkingInJava/exceptions/ExceptionMethods.java
@@ -0,0 +1,30 @@
+package com.christer.thinkingInJava.exceptions;//: exceptions/ExceptionMethods.java
+// Demonstrating the Exception Methods.
+import static net.mindview.util.Print.*;
+
+/**
+ * Exception类的各种方法的使用
+ */
+public class ExceptionMethods {
+  public static void main(String[] args) {
+    try {
+      throw new Exception("My Exception");
+    } catch(Exception e) {
+      print("Caught Exception");
+      print("getMessage():" + e.getMessage());
+      print("getLocalizedMessage():" +
+        e.getLocalizedMessage());
+      print("toString():" + e);
+      print("printStackTrace():");
+      e.printStackTrace(System.out);
+    }
+  }
+} /* Output:
+Caught Exception
+getMessage():My Exception
+getLocalizedMessage():My Exception
+toString():java.lang.Exception: My Exception
+printStackTrace():
+java.lang.Exception: My Exception
+        at ExceptionMethods.main(ExceptionMethods.java:8)
+*///:~

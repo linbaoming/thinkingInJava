9fb0b04a2ee3d36202a6c9d089d492006c8a183d
linbaoming <baoming_lin@163.com>
2017-02-22 00:17:45 +0800

    通过异常处理错误

--- src/main/java/com/christer/thinkingInJava/exceptions/NeverCaught.java
+++ src/main/java/com/christer/thinkingInJava/exceptions/NeverCaught.java
@@ -2,6 +2,9 @@ package com.christer.thinkingInJava.exceptions;//: exceptions/NeverCaught.java
 // Ignoring RuntimeExceptions.
 // {ThrowsException}
 
+/**
+ * RuntimeException如果没有被捕获而直达main()，那么在程序退出前将调用异常的printStackTrace方法
+ */
 public class NeverCaught {
   static void f() {
     throw new RuntimeException("From f()");

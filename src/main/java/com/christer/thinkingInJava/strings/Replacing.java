74eb3bd10ba076cb486ebc131a0e44e0ab8209c7
linbaoming <baoming_lin@163.com>
2017-02-23 00:39:06 +0800

    字符串

---
+++ src/main/java/com/christer/thinkingInJava/strings/Replacing.java
@@ -0,0 +1,16 @@
+package com.christer.thinkingInJava.strings;//: strings/Replacing.java
+import static net.mindview.util.Print.*;
+
+/**
+ * String.replace方法也是使用了正则匹配
+ */
+public class Replacing {
+  static String s = Splitting.knights;
+  public static void main(String[] args) {
+    print(s.replaceFirst("f\\w+", "located"));
+    print(s.replaceAll("shrubbery|tree|herring","banana"));
+  }
+} /* Output:
+Then, when you have located the shrubbery, you must cut down the mightiest tree in the forest... with... a herring!
+Then, when you have found the banana, you must cut down the mightiest banana in the forest... with... a banana!
+*///:~

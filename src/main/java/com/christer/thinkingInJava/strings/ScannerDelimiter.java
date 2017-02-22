8582bcd5219c7e51f5d18f37ecfe272a0906996c
linbaoming <baoming_lin@163.com>
2017-02-23 01:33:39 +0800

    字符串

--- src/main/java/com/christer/thinkingInJava/strings/ScannerDelimiter.java
+++ src/main/java/com/christer/thinkingInJava/strings/ScannerDelimiter.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class ScannerDelimiter {
   public static void main(String[] args) {
     Scanner scanner = new Scanner("12, 42, 78, 99, 42");
-    scanner.useDelimiter("\\s*,\\s*");
+    scanner.useDelimiter("\\s*,\\s*");// 设置定界符
     while(scanner.hasNextInt())
       System.out.println(scanner.nextInt());
   }

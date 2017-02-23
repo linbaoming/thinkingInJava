dee30cd76665c229d7ec1aeced44b6c24b854741
linbaoming <baoming_lin@163.com>
2017-02-23 23:38:05 +0800

    类型信息

---
+++ src/main/java/com/christer/thinkingInJava/typeinfo/ClassInitialization.java
@@ -0,0 +1,56 @@
+package com.christer.thinkingInJava.typeinfo;//: typeinfo/ClassInitialization.java
+import java.util.*;
+
+class Initable {
+  static final int staticFinal = 47;
+  static final int staticFinal2 =
+    ClassInitialization.rand.nextInt(1000);
+  static {
+    System.out.println("Initializing Initable");
+  }
+}
+
+class Initable2 {
+  static int staticNonFinal = 147;
+  static {
+    System.out.println("Initializing Initable2");
+  }
+}
+
+class Initable3 {
+  static int staticNonFinal = 74;
+  static {
+    System.out.println("Initializing Initable3");
+  }
+}
+
+/**
+ * 当使用.class来创建对Class对象的引用时，不会自动地初始化该Class对象
+ * 初始化会被毛延迟到对静态方法或者非常数静态域进行首次引用时才执行
+ */
+public class ClassInitialization {
+  public static Random rand = new Random(47);
+  public static void main(String[] args) throws Exception {
+    Class initable = Initable.class;
+    System.out.println("After creating Initable ref");
+    // Does not trigger initialization:
+    System.out.println(Initable.staticFinal);
+    // Does trigger initialization:
+    System.out.println(Initable.staticFinal2);
+    // Does trigger initialization:
+    System.out.println(Initable2.staticNonFinal);
+    Class initable3 = Class.forName("Initable3");
+    System.out.println("After creating Initable3 ref");
+    System.out.println(Initable3.staticNonFinal);
+  }
+} /* Output:
+After creating Initable ref
+47
+Initializing Initable
+258
+Initializing Initable2
+147
+Initializing Initable3
+After creating Initable3 ref
+74
+*///:~

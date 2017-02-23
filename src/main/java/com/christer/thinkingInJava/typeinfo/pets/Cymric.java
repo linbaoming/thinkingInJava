8c57b5ff24e9171083476ffb3b22a1c49c604219
linbaoming <baoming_lin@163.com>
2017-02-24 00:30:13 +0800

    类型信息

---
+++ src/main/java/com/christer/thinkingInJava/typeinfo/pets/Cymric.java
@@ -0,0 +1,7 @@
+//: typeinfo/pets/Cymric.java
+package com.christer.thinkingInJava.typeinfo.pets;
+
+public class Cymric extends Manx {
+  public Cymric(String name) { super(name); }
+  public Cymric() { super(); }
+} ///:~
---
+++ src/main/java/com/christer/thinkingInJava/typeinfo/pets/Person.java
@@ -0,0 +1,6 @@
+//: typeinfo/pets/Person.java
+package com.christer.thinkingInJava.typeinfo.pets;
+
+public class Person extends Individual {
+  public Person(String name) { super(name); }
+} ///:~

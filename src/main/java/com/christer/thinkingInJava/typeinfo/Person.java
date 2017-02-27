fd14f0a97f6eb094977a1dfc5785fe3fd1f73d28
linbaoming <baoming_lin@163.com>
2017-02-28 00:34:43 +0800

    类型信息

---
+++ src/main/java/com/christer/thinkingInJava/typeinfo/Person.java
@@ -0,0 +1,27 @@
+package com.christer.thinkingInJava.typeinfo;//: typeinfo/Person.java
+// A class with a Null Object.
+import net.mindview.util.*;
+
+/**
+ * 空对象，空对象都是单例
+ */
+class Person {
+  public final String first;
+  public final String last;
+  public final String address;
+  // etc.
+  public Person(String first, String last, String address){
+    this.first = first;
+    this.last = last;
+    this.address = address;
+  }	
+  public String toString() {
+    return "Person: " + first + " " + last + " " + address;
+  }
+  public static class NullPerson
+  extends Person implements Null {
+    private NullPerson() { super("None", "None", "None"); }
+    public String toString() { return "NullPerson"; }
+  }
+  public static final Person NULL = new NullPerson();
+} ///:~

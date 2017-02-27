6164ad27d97b513d97843301219379ebdb1d4439
linbaoming <baoming_lin@163.com>
2017-02-27 23:51:02 +0800

    类型信息

---
+++ src/main/java/com/christer/thinkingInJava/typeinfo/PetCount2.java
@@ -0,0 +1,8 @@
+package com.christer.thinkingInJava.typeinfo;//: typeinfo/PetCount2.java
+import com.christer.thinkingInJava.typeinfo.pets.*;
+
+public class PetCount2 {
+  public static void main(String[] args) {
+    PetCount.countPets(Pets.creator);
+  }
+} /* (Execute to see output) *///:~

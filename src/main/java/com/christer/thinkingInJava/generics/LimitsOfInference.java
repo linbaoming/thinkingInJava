c8e08b74e1255452f5b123c41c831c95ffe5de87
linbaoming <baoming_lin@163.com>
2017-03-01 00:39:51 +0800

    泛型

---
+++ src/main/java/com/christer/thinkingInJava/generics/LimitsOfInference.java
@@ -0,0 +1,16 @@
+package com.christer.thinkingInJava.generics;//: generics/LimitsOfInference.java
+import com.christer.thinkingInJava.typeinfo.pets.*;
+import java.util.*;
+
+/**
+ * 类型参数推导只对赋值操作有效，其它时候并不起作用
+ * 赋值 可以通过声明的变量确定泛型类型
+ * 方法参数 只是传值，不能倒过来用参数类型来确定泛型类型
+ */
+public class LimitsOfInference {
+  static void
+  f(Map<Person, List<? extends Pet>> petPeople) {}
+  public static void main(String[] args) {
+    // f(New.map()); // Does not compile
+  }
+} ///:~

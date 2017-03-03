56f1aae8ba2fefe452db9d5111c94b99ff17ec44
linbaoming <baoming_lin@163.com>
2017-03-03 17:22:11 +0800

    泛型

---
+++ src/main/java/com/christer/thinkingInJava/generics/CountedObject.java
@@ -0,0 +1,8 @@
+package com.christer.thinkingInJava.generics;//: generics/CountedObject.java
+
+public class CountedObject {
+  private static long counter = 0;
+  private final long id = counter++;
+  public long id() { return id; }
+  public String toString() { return "CountedObject " + id;}
+} ///:~

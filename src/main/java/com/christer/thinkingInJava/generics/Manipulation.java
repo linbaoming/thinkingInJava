67e44b68af29de6e27b044aa639ed01a14e578d8
linbaoming <baoming_lin@163.com>
2017-03-03 18:04:17 +0800

    泛型

--- src/main/java/com/christer/thinkingInJava/generics/Manipulation.java
+++ src/main/java/com/christer/thinkingInJava/generics/Manipulation.java
@@ -1,6 +1,9 @@
 package com.christer.thinkingInJava.generics;//: generics/Manipulation.java
 // {CompileTimeError} (Won't compile)
 
+/**
+ * 因为擦除的关系，所以无法调用
+ */
 class Manipulator<T> {
   private T obj;
   public Manipulator(T x) { obj = x; }

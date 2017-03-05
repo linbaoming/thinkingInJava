4067d96e5e73792a7d30eb1611a073e77bed60e1
linbaoming <baoming_lin@163.com>
2017-03-05 22:47:21 +0800

    泛型

---
+++ src/main/java/com/christer/thinkingInJava/generics/ArrayOfGenericReference.java
@@ -0,0 +1,7 @@
+package com.christer.thinkingInJava.generics;//: generics/ArrayOfGenericReference.java
+
+class Generic<T> {}
+
+public class ArrayOfGenericReference {
+  static Generic<Integer>[] gia;
+} ///:~

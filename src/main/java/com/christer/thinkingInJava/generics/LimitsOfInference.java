699e74565829a04b48b3be772ade094dbb105e78
linbaoming <baoming_lin@163.com>
2017-03-14 01:58:00 +0800

    泛型

--- src/main/java/com/christer/thinkingInJava/generics/LimitsOfInference.java
+++ src/main/java/com/christer/thinkingInJava/generics/LimitsOfInference.java
@@ -11,6 +11,7 @@ public class LimitsOfInference {
   static void
   f(Map<Person, List<? extends Pet>> petPeople) {}
   public static void main(String[] args) {
-    // f(New.map()); // Does not compile
+    // f(New.map()); // Does not compile 无法推断map里的类型
+    f(new HashMap<Person, List<? extends Pet>>());
   }
 } ///:~
